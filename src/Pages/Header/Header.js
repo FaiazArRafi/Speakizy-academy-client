7121aff230387d263146cf99c445ba71dcf80d3c
Faiaz <faiazarrafi@gmail.com>
2022-10-27 16:32:23 +0600

    navbar

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <div>
             <Navbar bg="light" variant="light">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#home">Speakizy Academy</Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#features">Features</Nav.Link>
