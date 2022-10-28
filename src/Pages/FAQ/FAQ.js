ecba64d53c2f07afdef731361d99c84a02027192
Faiaz <faiazarrafi@gmail.com>
2022-10-28 23:35:35 +0600

    FAQ added

--- src/Pages/FAQ/FAQ.js
+++ src/Pages/FAQ/FAQ.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 const FAQ = () => {
     return (
-        <div>
-
+        <div className='container my-2 p-3 w-50'>
+            <div className='bg-info text-dark p-3 rounded-3 mb-3'>
+                <h4>Q: Why learn a language ?</h4>
+                <p>People learn languages for a number of reasons: some for work or business, some for education or to put on their CV’s, and some merely for the pleasure of being able to communicate with different people and cultures. Any trip abroad is greatly enhanced if you can say a few words, let alone if you can actually have a conversation with the locals!</p>
+            </div>
+            <div className='bg-info text-dark p-3 rounded-3 mb-3'>
+                <h4>Q: How much will I learn during my course?</h4>
+                <p>Speakizy Academy gives an overview of the content and learning that include themes, grammar and vocabulary you can typically expect to cover. These are intended as a guide only and our teachers may at times adapt the course content to suit the specific level, aims and interests of the class.</p>
+            </div>
+            <div className='bg-info text-dark p-3 rounded-3 mb-3'>
+                <h4>Q: What progress will I have made by the end of the level ?</h4>
+                <p>Speakizy Academy provides a guide as to what you can expect to achieve at each level and also show what progress you can expect to make if you stick with it and carry on studying subsequent levels. These are guides only and other factors will affect your rate of progress, e.g. your prior language skills and base level, how proficient you are at picking up new languages, your level of participation in class and revision (and homework) outside of class. It will partially also depend on how difficult (or dissimilar) the language you are learning is (from your native language).</p>
+            </div>
         </div>
     );
 };
