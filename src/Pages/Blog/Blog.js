60f65d2def346f5ee99198839c5913d5d65a40cf
Faiaz <faiazarrafi@gmail.com>
2022-10-28 21:56:41 +0600

    blog added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -9,20 +9,27 @@ const Blog = () => {
             </div>
             <div className='bg-success text-white p-3 rounded-3 mb-3'>
                 <h4>Q: Why are you using firebase? What other options do you have to implement authentication?</h4>
-                <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. <br />
-                    React.createContext() returns a consumer and a provider. "Provider" is a component that provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. "Consumer" as it happens is a component that consumes and uses the state.
+                <p>Firebase one of the key feature is realtime database. The Realtime Database is essentially a cloud-hosted NoSQL database that stores data as JSON (JavaScript Object Notation) in real time. One of the key advantages for Firebase’s Realtime Database is that it works offline by using local cache on the device to store any changes made. When app connectivity is resumed, the data is synced. <br />
+                    Most apps have some form of authentication feature that is necessary to identify the user. Thanks to Firebase’s easy-to-use SDK, ready-made UI libraries, and backend services, it is possible to set up the whole authentication process in just a couple of minutes. Firebase allows users to authenticate with their phone number, email, Google, and Facebook accounts and many more. <br />
+                    Google Analytics for Firebase allows us to track your users’ journey through realtime and custom reporting. Firebase supports IOS, Android, Web, and Unity products, allowing us to track our users across a wide range of devices. According to Google, Firebase provides unlimited free reporting on up to 500 distinct events. Just like the regular Google Analytics, Google Analytics for Firebase automatically tracks certain key events and user parameters straight out of the box, and allows us to define custom events that are important to our application.<br /><br />
+                    Some Firebase alternatives<br />
+                    1 Back4App <br />
+                    2 Backendless <br />
+                    3 Kuzzle <br />
+                    4 Pubnub <br />
+                    5 Kumulos <br />
                 </p>
             </div>
             <div className='bg-success text-white p-3 rounded-3 mb-3'>
                 <h4>Q: How does the private route work ?</h4>
-                <p>The useRef is a hook that allows to directly create a reference to the DOM element in the functional component. <br />
-                    The useRef returns a mutable ref object. This object has a property called ".current". The value is persisted in the refContainer.current property. These values are accessed from the current property of the returned object. The ".current" property could be initialised to the passed argument initialValue e.g. useRef(initialValue). The object can persist a value for a full lifetime of the component.
+                <p>Private Routes in React Router (also called Protected Routes) require a user being authorized to visit a route (read: page). So if a user is not authorized for a specific page, they cannot access it. The most common example is authentication in a React application where a user can only access the protected pages when they are authorized (which means in this case being authenticated). Authorization goes beyond authentication though. For example, a user can also have roles and permissions which give a user access to specific areas of the application.
                 </p>
             </div>
             <div className='bg-success text-white p-3 rounded-3 mb-3'>
                 <h4>Q: What is Node? How does Node work?</h4>
-                <p>The useRef is a hook that allows to directly create a reference to the DOM element in the functional component. <br />
-                    The useRef returns a mutable ref object. This object has a property called ".current". The value is persisted in the refContainer.current property. These values are accessed from the current property of the returned object. The ".current" property could be initialised to the passed argument initialValue e.g. useRef(initialValue). The object can persist a value for a full lifetime of the component.
+                <p>Node (or more formally Node.js) is an open-source, cross-platform runtime environment that allows developers to create all kinds of server-side tools and applications in JavaScript.<br />
+                    Node is completely event-driven. Basically the server consists of one thread processing one event after another.<br />
+                    A new request coming in is one kind of event. The server starts processing it and when there is a blocking IO operation, it does not wait until it completes and instead registers a callback function. The server then immediately starts to process another event (maybe another request). When the IO operation is finished, that is another kind of event, and the server will process it (i.e. continue working on the request) by executing the callback as soon as it has time.
                 </p>
             </div>
         </div>
